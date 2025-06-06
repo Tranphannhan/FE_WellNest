b1e77ee841759e5becaca321b192403a4c8792ad
TamPhamLor <congtam002005@gmail.con>
2025-06-06 16:20:46 +0700

    Làm layout Form nhập tay với Tạo sổ khám bệnh tạm thời

--- src/app/Receptionist/Reception/inputForm/page.tsx
+++ src/app/Receptionist/Reception/inputForm/page.tsx
@@ -1,5 +1,5 @@
 import Tabbar from "@/app/components/shared/Tabbar/Tabbar";
-
+import './inputForm.css'
 export default function InputForm() {
     return (
         <>
@@ -14,17 +14,66 @@ export default function InputForm() {
                
             }
         ></Tabbar>
-            <div className="input-form">
-                <h1>Input Form</h1>
-                <form>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" name="name" required />
-                    
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" required />
-                    
-                    <button type="submit">Submit</button>
-                </form>
+            <div className="form__container">
+            <form>
+                <div className="row">
+                    <div className="form__group">
+                        <label><span className="text__red">*</span>Họ và tên:</label>
+                        <input type="text" />
+                    </div>
+                <div className="form__group">
+                        <label>Số CCCD (Không bắt buộc):</label>
+                        <input type="text" />
+                    </div>
+                <div className="form__group">
+                        <label><span className="text__red">*</span>Ngày sinh:</label>
+                        <input type="date" style={{color:'#808080'}}/>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="form__group">
+                        <label>Số BHYT (Không bắt buộc):</label>
+                        <input type="text" />
+                    </div>
+                <div className="form__group">
+                        <label>Số điện thoại (Không bắt buộc):</label>
+                        <input type="text" />
+                    </div>
+                <div className="form__group">
+                        <label>Số điện thoại người thân (Không bắt buộc):</label>
+                        <input type="text" />
+                    </div>
+                </div>  
+
+                <div className="row">
+                <div className="form__group">
+                    <label><span className="text__red">*</span>Giới tính:</label>
+                    <div className="gender">
+                    <label>
+                        <input className="gender__input" type="radio" name="gender" defaultChecked /> Nam
+                    </label>
+                    <label>
+                        <input className="gender__input" type="radio" name="gender" /> Nữ
+                    </label>
+                    </div>
+                </div>
+                <div className="form__group address__group">
+                    <label>Địa chỉ (Không bắt buộc):</label>
+                    <textarea rows={3}></textarea>
+                </div>
+                </div>
+
+                <div className="form__group">
+                <label>Lịch sử bệnh (Không bắt buộc):</label>
+                <textarea rows={4}></textarea>
+                </div>
+
+                <div className="Form_button">
+                    <button>Tạo sổ khám bệnh tạm thời</button>
+                </div>
+                
+            </form>
             </div>
         </>
     );
