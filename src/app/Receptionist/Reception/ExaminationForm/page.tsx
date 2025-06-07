bf213fd5f672283375a06e9dda469036967dd75d
TamPhamLor <congtam002005@gmail.con>
2025-06-07 13:24:12 +0700

    update className

--- src/app/Receptionist/Reception/ExaminationForm/page.tsx
+++ src/app/Receptionist/Reception/ExaminationForm/page.tsx
@@ -13,28 +13,29 @@ export default function ExaminationForm(){
             }
     >
     </Tabbar>
-            <div className="page-wrapper">
-                <div className="ExaminationForm">
-                    <div className="form-header">
+                <div className="ExaminationForm-Container">
+                    <div className="ExaminationForm-Container__header">
                         <h2>Thông tin phiếu khám</h2>
-                        <button className="accept-btn">🖨 In phiếu khám</button>
+                        <div className="ExaminationForm-Container__print">
+                        <button className="ExaminationForm-Container__print__btn">🖨 In phiếu khám</button>
+                        </div>
                     </div>
 
                     {/* Khung 1: Thông tin cơ bản (4 cột) */}
                     <div className="form-grid grid-4">
-                        <div className="form-group">
+                        <div className="ExaminationForm-Container__form__group">
                             <label htmlFor="fullName">Họ và tên:</label>
                             <input type="text" id="fullName" defaultValue="Lý Văn Điền" readOnly/>
                         </div>
-                        <div className="form-group">
+                        <div className="ExaminationForm-Container__form__group">
                             <label htmlFor="cccd">Số CCCD:</label>
                             <input type="text" id="cccd" defaultValue="080205013878" readOnly />
                         </div>
-                        <div className="form-group">
+                        <div className="ExaminationForm-Container__form__group">
                             <label htmlFor="dob">Ngày sinh:</label>
                             <input type="text" id="dob" defaultValue="16/08/1998" readOnly />
                         </div>
-                       <div className="form-group">
+                       <div className="ExaminationForm-Container__form__group">
                             <label htmlFor="phone">Số điện thoại:</label>
                             <input type="text" id="phone" defaultValue="0343527854" readOnly/>
                         </div>
@@ -42,7 +43,7 @@ export default function ExaminationForm(){
 
                     {/* Khung 2: Thông tin chi tiết (4 cột) */}
                     <div className="form-grid grid-3">
-                         <div className="form-group">
+                         <div className="ExaminationForm-Container__form__group">
                             <label htmlFor="gender">Giới tính:</label>
                             {/* Giới tính dùng input readonly để giống hình ảnh */}
                             <input type="text" id="gender" defaultValue="Nam" readOnly className="gender-btn" />
@@ -50,29 +51,29 @@ export default function ExaminationForm(){
                         
 
                         {/* Chiều cao */}
-                        <div className="form-group">
+                        <div className="ExaminationForm-Container__form__group">
                             <label htmlFor="height">Chiều cao:</label>
-                            <div className="input-unit">
+                            <div className="ExaminationForm-Container__input__unit">
                                 <input type="text" id="height" defaultValue="173" readOnly/>
                                 <span>Cm</span>
                             </div>
                         </div>
 
                         {/* Cân nặng */}
-                        <div className="form-group">
+                        <div className="ExaminationForm-Container__form__group">
                             <label htmlFor="weight">Cân nặng:</label>
-                            <div className="input-unit">
+                            <div className="ExaminationForm-Container__input__unit">
                                 <input type="text" id="weight" defaultValue="61" readOnly/>
                                 <span>Kg</span>
                             </div>
                         </div>
 
-                        <div className="form-group">
+                        <div className="ExaminationForm-Container__form__group">
                             <label htmlFor="clinic">Phòng khám:</label>
                             <input type="text" id="clinic" defaultValue="1" readOnly/>
                         </div>
 
-                        <div className="form-group">
+                        <div className="ExaminationForm-Container__form__group">
                             <label htmlFor="department">Khoa:</label>
                             <input type="text" id="department" defaultValue="Chấn thương chỉnh hình" readOnly/>
                         </div>
@@ -80,21 +81,20 @@ export default function ExaminationForm(){
 
                     {/* Khung 3: Địa chỉ và Lý do đến khám (2 cột) */}
                     <div className="form-grid grid-2">
-                        <div className="form-group">
+                        <div className="ExaminationForm-Container__form__group">
                             <label htmlFor="address">Địa chỉ:</label>
                             <textarea id="address" readOnly defaultValue="403, ấp Mỹ Điền, xã Long Hựu, huyện Cần Đước,tỉnh Long An" />
                         </div>
-                        <div className="form-group">
+                        <div className="ExaminationForm-Container__form__group">
                             <label htmlFor="reason">Lí do đến khám:</label>
                             <textarea id="reason" readOnly defaultValue="Nứt xương cánh tay" />
                         </div>
                     </div>
 
-                    <div className="print-btn">
-                        <button>Xác nhận đã thanh toán</button>
+                    <div className="ExaminationForm-Container__accept">
+                        <button className="ExaminationForm-Container__accept__btn">Xác nhận đã thanh toán</button>
                     </div>
                 </div>
-            </div>
         </>
     
     )
--- src/app/Receptionist/Reception/inputForm/page.tsx
+++ src/app/Receptionist/Reception/inputForm/page.tsx
@@ -14,42 +14,42 @@ export default function InputForm() {
                
             }
         ></Tabbar>
-            <div className="InputForm__Container">
+            <div className="InputForm-Container">
             <form>
-                <div className="row">
-                    <div className="form__group">
+                <div className="InputForm-Container__row">
+                    <div className="InputForm-Container__form__group">
                         <label><span className="text__red">*</span>Họ và tên:</label>
                         <input type="text" />
                     </div>
-                <div className="form__group">
+                <div className="InputForm-Container__form__group">
                         <label>Số CCCD (Không bắt buộc):</label>
                         <input type="text" />
                     </div>
-                <div className="form__group">
+                <div className="InputForm-Container__form__group">
                         <label><span className="text__red">*</span>Ngày sinh:</label>
                         <input type="date" style={{color:'#808080'}}/>
                     </div>
                 </div>
 
-                <div className="row">
-                    <div className="form__group">
+                <div className="InputForm-Container__row">
+                    <div className="InputForm-Container__form__group">
                         <label>Số BHYT (Không bắt buộc):</label>
                         <input type="text" />
                     </div>
-                <div className="form__group">
+                <div className="InputForm-Container__form__group">
                         <label>Số điện thoại (Không bắt buộc):</label>
                         <input type="text" />
                     </div>
-                <div className="form__group">
+                <div className="InputForm-Container__form__group">
                         <label>Số điện thoại người thân (Không bắt buộc):</label>
                         <input type="text" />
                     </div>
                 </div>  
 
-                <div className="row">
-                <div className="form__group">
+                <div className="InputForm-Container__row">
+                <div className="InputForm-Container__form__group">
                     <label><span className="text__red">*</span>Giới tính:</label>
-                    <div className="gender">
+                    <div className="InputForm-Container__gender">
                     <label>
                         <input className="gender__input" type="radio" name="gender" defaultChecked /> Nam
                     </label>
@@ -58,18 +58,18 @@ export default function InputForm() {
                     </label>
                     </div>
                 </div>
-                <div className="form__group address__group">
+                <div className="InputForm-Container__form__group InputForm-Container__address__group">
                     <label>Địa chỉ (Không bắt buộc):</label>
                     <textarea rows={3}></textarea>
                 </div>
                 </div>
 
-                <div className="form__group">
+                <div className="InputForm-Container__form__group">
                 <label>Lịch sử bệnh (Không bắt buộc):</label>
                 <textarea rows={4}></textarea>
                 </div>
 
-                <div className="Form_button">
+                <div className="InputForm-Container__form__button">
                     <button>Tạo sổ khám bệnh tạm thời</button>
                 </div>
                 
