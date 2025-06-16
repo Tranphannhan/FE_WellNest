aa36e0a101c2852fb4898771e7f75dba264b77a3
TranPhanNhan <nhannhaongo1997@gmail.com>
2025-06-16 14:06:27 +0700

    Lưu

--- src/app/services/DoctorSevices.ts
+++ src/app/services/DoctorSevices.ts
@@ -290,7 +290,7 @@ export async function CheckPrescription(Id_PhieuKhamBenh: string) {
     const response = await fetch(`http://localhost:5000/Donthuoc/KiemTraDonThuocDangTao?Id_PhieuKhamBenh=${Id_PhieuKhamBenh}`);
 
     if (!response.ok) {
-      console.error("Không lấy được chi tiết phiếu khám bệnh");
+      console.error("Lỗi khi kiểm tra đơn thuốc");
       return { status: false, data: null };
     }
 
@@ -304,8 +304,8 @@ export async function CheckPrescription(Id_PhieuKhamBenh: string) {
     }
 
   } catch (error) {
-    console.error("Lỗi khi fetch chi tiết phiếu khám bệnh:", error);
-    return { status: false, data: null };
+    console.error("Lỗi khi kiểm tra đơn thuốc:", error);
+    throw Error('Lỗi khi kiểm tra đơn thuốc')
   }
 }
 
