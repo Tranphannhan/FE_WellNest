fb1453a47348c79600642433201acea1832616ac
TranPhanNhan <nhannhaongo1997@gmail.com>
2025-07-02 13:08:18 +0700

    Lưu

--- src/app/Admin/HumanResources/Doctor/page.tsx
+++ src/app/Admin/HumanResources/Doctor/page.tsx
@@ -191,9 +191,9 @@ export default function Page() {
         <CustomTableHumanResources
           columns={columns}
           rows={filteredRows}
-          onEdit={() => {}}
+          onEdit={(id) => {}}
           onDelete={() => {}}
-          onDisable={() => {}}
+          onDisable={(id) => {}}
           showEdit={true}
           showDelete={false}
           showDisable={true}
